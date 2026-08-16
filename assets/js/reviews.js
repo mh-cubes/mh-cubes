cba9c30616304192fd504d0d83608532e0752585
mh-cubes <huzaifa81102@gmail.com>
2026-08-16 20:52:36 +0500

    Update reviews.js

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -9,7 +9,7 @@ import {
     where
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-import { db } from "./firebase.js";
+import { db } from "../../firebase.js";
 
 
 document.addEventListener("DOMContentLoaded", function () {
