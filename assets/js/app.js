183547be009aa3ded83aafde71fde3d18dd84e22
mh-cubes <huzaifa81102@gmail.com>
2026-08-21 14:36:49 +0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-import { auth } from "../../firebase.js";
+import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 document.addEventListener("DOMContentLoaded", () => {
 
